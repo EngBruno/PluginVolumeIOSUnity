e6f85708e08515d27ddbb0f8707ae453ae9b99a0
Bruno <bruno@iMac-de-Acessibilidade.local>
2015-08-14 12:38:01 -0300

    Initial Commit

---
+++ VolumeIOSUnity/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VolumeIOSUnity
+//
+//  Created by Bruno on 14/08/15.
+//  Copyright (c) 2015 Bruno. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VolumeIOSUnity/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VolumeIOSUnity
+//
+//  Created by Bruno on 14/08/15.
+//  Copyright (c) 2015 Bruno. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
